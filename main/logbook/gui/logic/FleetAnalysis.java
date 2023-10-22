8c77b87a52b5438fef98ab1efeb708d084a21567
Nishikuma <nishikuma.develop@gmail.com>
2023-10-22 19:54:00 +0900

    反映先ラジオボタンの挙動がイマイチだったのでフォーマット修正

--- main/logbook/gui/logic/FleetAnalysis.java
+++ main/logbook/gui/logic/FleetAnalysis.java
@@ -24,35 +24,40 @@ public class FleetAnalysis {
     public String getShipsFormat() {
         List<ShipDto> ships = GlobalContext.getShipMap().values().stream().collect(Collectors.toList());
 
-        return "[" + ships.stream().map(ShipDto::getJson)
-                .filter(Objects::nonNull)
-                .map(json -> {
-                    JsonObjectBuilder result = Json.createObjectBuilder();
-                    if (json.containsKey("api_ship_id")) {
-                        result = result.add("api_ship_id", json.getInt("api_ship_id"));
-                    }
-                    if (json.containsKey("api_lv")) {
-                        result = result.add("api_lv", json.getInt("api_lv"));
-                    }
-                    if (json.containsKey("api_kyouka")) {
-                        result = result.add("api_kyouka", json.getJsonArray("api_kyouka"));
-                    }
-                    if (json.containsKey("api_exp")) {
-                        result = result.add("api_exp", json.getJsonArray("api_exp"));
-                    }
-                    if (json.containsKey("api_slot_ex")) {
-                        result = result.add("api_slot_ex", json.getInt("api_slot_ex"));
-                    }
-                    if (json.containsKey("api_sally_area")) {
-                        result = result.add("api_sally_area", json.getInt("api_sally_area"));
-                    }
-                    if (json.containsKey("api_locked")) {
-                        result = result.add("api_locked", json.getInt("api_locked"));
-                    }
+        return "svdata={\"api_result\":1,\"api_result_msg\":\"成功\",\"api_data\":{\"api_ship\":["
+                + ships.stream().map(ShipDto::getJson)
+                        .filter(Objects::nonNull)
+                        .map(json -> {
+                            JsonObjectBuilder result = Json.createObjectBuilder();
+                            if (json.containsKey("api_id")) {
+                                result = result.add("api_id", json.getInt("api_id"));
+                            }
+                            if (json.containsKey("api_ship_id")) {
+                                result = result.add("api_ship_id", json.getInt("api_ship_id"));
+                            }
+                            if (json.containsKey("api_lv")) {
+                                result = result.add("api_lv", json.getInt("api_lv"));
+                            }
+                            if (json.containsKey("api_kyouka")) {
+                                result = result.add("api_kyouka", json.getJsonArray("api_kyouka"));
+                            }
+                            if (json.containsKey("api_exp")) {
+                                result = result.add("api_exp", json.getJsonArray("api_exp"));
+                            }
+                            if (json.containsKey("api_slot_ex")) {
+                                result = result.add("api_slot_ex", json.getInt("api_slot_ex"));
+                            }
+                            if (json.containsKey("api_sally_area")) {
+                                result = result.add("api_sally_area", json.getInt("api_sally_area"));
+                            }
+                            if (json.containsKey("api_locked")) {
+                                result = result.add("api_locked", json.getInt("api_locked"));
+                            }
 
-                    return result.build().toString();
-                })
-                .collect(Collectors.joining(",")) + "]";
+                            return result.build().toString();
+                        })
+                        .collect(Collectors.joining(","))
+                + "]}}";
     }
 
     /**
@@ -63,10 +68,12 @@ public class FleetAnalysis {
     public String getItemsFormat() {
         List<ItemDto> items = GlobalContext.getItemMap().values().stream().collect(Collectors.toList());
 
-        return "[" + items.stream().map(item -> Json.createObjectBuilder()
-                .add("api_slotitem_id", item.getSlotitemId())
-                .add("api_level", item.getLevel())
-                .add("api_locked", item.isLocked() ? 1 : 0).build().toString()).collect(Collectors.joining(","))
-                + "]";
+        return "svdata={\"api_result\":1,\"api_result_msg\":\"成功\",\"api_data\":["
+                + items.stream().map(item -> Json.createObjectBuilder()
+                        .add("api_id", item.getId())
+                        .add("api_slotitem_id", item.getSlotitemId())
+                        .add("api_level", item.getLevel())
+                        .add("api_locked", item.isLocked() ? 1 : 0).build().toString()).collect(Collectors.joining(","))
+                + "]}";
     }
 }
